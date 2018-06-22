7620c2140b4f767b7f0a131c1e6c8b5ccf595597
mlprajapati <madanmannu@gmail.com>
2018-06-22 17:33:57 +0530

    angular6-mapbox-poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,12 @@ export class AppComponent implements OnInit, OnDestroy {
   pitch: number;
   coord: LngLatLike;
   showMarker: boolean = false;
+  initialAngel:number=0;
+  rotateMarker = {
+    '-ms-transform': 'rotate(-20deg)',
+    '-webkit-transform': 'rotate(-20deg)',
+    'transform': 'rotate(-20deg)'
+  }
   private timer: number;
 
   feature: any = {
@@ -41,6 +47,7 @@ export class AppComponent implements OnInit, OnDestroy {
       this.http.get('../assets/hike.geo.json').subscribe(response => {
       const data: GeoJSON.FeatureCollection<GeoJSON.LineString> = <any>response;
       const coordinates = data.features[0].geometry!.coordinates;
+      //data.features[0].geometry!.coordinates = [coordinates[0]];
       data.features[0].geometry!.coordinates = coordinates;
       this.coord = coordinates[0];
       this.feature.geometry!.coordinates = coordinates[0];
@@ -51,11 +58,17 @@ export class AppComponent implements OnInit, OnDestroy {
       this.showMarker = true;
       let i = 0;
       this.timer = window.setInterval(() => {
-        if (i < coordinates.length) {
+        if (i < coordinates.length-1) {
           // this.center = coordinates[i];
          // data.features[0].geometry!.coordinates.push(coordinates[i]);
+         if(i<coordinates.length){
+          this.rotateMarker["-ms-transform"] = 'rotate('+this.calculateAngel(coordinates[i][0],coordinates[i][1])+'deg)';
+          this.rotateMarker["-webkit-transform"] = 'rotate('+this.calculateAngel(coordinates[i][0],coordinates[i][1])+'deg)';
+          this.rotateMarker.transform = 'rotate('+this.calculateAngel(coordinates[i][0],coordinates[i][1],)+'deg)';
+          this.rotateMarker = Object.assign({},this.rotateMarker);
           this.feature.geometry!.coordinates = coordinates[i];
           this.feature = Object.assign({}, this.feature);
+         }
           this.coord = coordinates[i];
           this.data = { ...this.data };
           i++;
@@ -66,6 +79,17 @@ export class AppComponent implements OnInit, OnDestroy {
       }, 10);
     });
   }
+  private calculateAngel(lat1:number,lon1:number){
+    var angle =0
+    lat1 += Math.cos(this.initialAngel) / 100;
+    lon1 += Math.sin(this.initialAngel) / 100;
+    angle = this.initialAngel * (180 / Math.PI);
+     if (Math.random() > 0.95) {
+      this.initialAngel += (Math.random() - 0.5) / 2;
+     }
+     return angle;
+  }
+  
   ngOnDestroy() {
     window.clearInterval(this.timer);
   }
