64211915a13c5c562fd8c4badacd95620cafc9e6
Madan Lalprajapati <madan.l@HCL.COM>
2018-07-13 16:17:25 +0530

    POC

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,11 +6,13 @@ import { Pages } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import {HeaderComponent} from '../shared/components/header/header.component';
 import { GreetingsComponent } from './greetings/greetings.component';
+import {FooterComponent} from '../shared/components/footer/footer.component';
+import {NotificationComponent} from '../shared/directives/notification/notification.directive';
 
 
 @NgModule({
   imports: [CommonModule, AppRoutingModule,SharedModule],
-  declarations: [Pages,HeaderComponent,GreetingsComponent]
+  declarations: [Pages,HeaderComponent,GreetingsComponent,FooterComponent,NotificationComponent]
 })
 export class PagesModule {
 }
