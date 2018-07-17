5ec039cdea6e7f19c43a6806d410bfdcdea76a81
Madan Lalprajapati <madan.l@HCL.COM>
2018-07-17 18:16:12 +0530

    netlily.toml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  accessTokenMapbox: 'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w',
+  accessTokenMapbox: 'pk.eyJ1IjoiY2hpdHRyYW5nIiwiYSI6ImNqanA3dHA5NTd3MjEzcGxmdDV0Njc4NXQifQ.NapWZkvR3_lYOqXQkZg99A',//'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w',
   directionsURL : 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/'
 };
